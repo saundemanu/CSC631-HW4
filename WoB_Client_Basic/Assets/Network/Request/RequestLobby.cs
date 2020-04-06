10e17a695c7d5322608e9d19559e38d4fe94a203
andrewcopas <andacrews@gmail.com>
2020-04-05 18:00:20 -0700

    added the client-side with some protocols and a mostly empty scene, also tried to add some things to gitignore

---
+++ WoB_Client_Basic/Assets/Network/Request/RequestLobby.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+using System;
+
+public class RequestLobby : NetworkRequest {
+
+	public RequestLobby() {
+		request_id = Constants.CMSG_AUTH;
+	}
+	
+	public void send(string lobby, string username, string password) {
+	    packet = new GamePacket(request_id);
+		packet.addString(lobby);
+        packet.addString(username);
+		packet.addString(password);
+	}
+}
---
+++ WoB_Client_Basic/Assets/Scripts/Constants.cs
@@ -0,0 +1,38 @@
+public class Constants {
+	
+	// Constants
+	public static readonly string CLIENT_VERSION = "1.00";
+	public static readonly string REMOTE_HOST = "localhost";
+	public static readonly int REMOTE_PORT = 9252;
+	
+	// Request (1xx) + Response (2xx)
+	public static readonly short CMSG_AUTH = 101;
+	public static readonly short SMSG_AUTH = 201;
+    public static readonly short CMSG_HEARTBEAT = 102;
+    public static readonly short SMSG_HEARTBEAT = 202;
+	public static readonly short CMSG_PLAYERS = 103;
+	public static readonly short SMSG_PLAYERS = 203;
+	public static readonly short CMSG_TEST = 104;
+	public static readonly short SMSG_TEST = 204;
+	public static readonly short CMSG_LOBBY = 105;
+	public static readonly short SMSG_LOBBY = 205;
+	//public static readonly short CMSG_ = 106;
+	//public static readonly short SMSG_ = 206;
+	//public static readonly short CMSG_ = 107;
+	//public static readonly short SMSG_ = 207;
+	//public static readonly short CMSG_ = 108;
+	//public static readonly short SMSG_ = 208;
+
+	
+	// Other
+	public static readonly string IMAGE_RESOURCES_PATH = "Images/";
+	public static readonly string PREFAB_RESOURCES_PATH = "Prefabs/";
+	public static readonly string TEXTURE_RESOURCES_PATH = "Textures/";
+	
+	// GUI Window IDs
+	public enum GUI_ID {
+		Login
+	};
+
+	public static int USER_ID = -1;
+}
